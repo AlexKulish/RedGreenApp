673222d3eeabb1290beb9e7e2360bdb6410240f1
Alex <alexkulish1993@gmail.com>
2021-12-07 09:23:26 +0300

    Initial Commit

---
+++ RedGreenApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedGreenApp
+//
+//  Created by Alex Kulish on 07.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
